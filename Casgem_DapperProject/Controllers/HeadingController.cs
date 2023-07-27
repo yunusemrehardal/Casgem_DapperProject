9f30edbd429d6e96e2e803ae053e9704fdb5730a
Yunus Emre Hardal <hardaalyunusemre@gmail.com>
2023-07-27 15:59:11 +0300

    crud

--- Casgem_DapperProject/Controllers/HeadingController.cs
+++ Casgem_DapperProject/Controllers/HeadingController.cs
@@ -14,5 +14,41 @@ namespace Casgem_DapperProject.Controllers
             var values = await connection.QueryAsync<Headings>("Select * From TblHeading");
             return View(values);
         }
+        [HttpGet]
+        public IActionResult AddHeading()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddHeading(Headings headings)
+        {
+            await using var connection = new SqlConnection(_connectionString);
+            var query = $"INSERT INTO TblHeading (HeadingName, HeadingStatus) Values ('{headings.HeadingName}', 'True')";
+            await connection.QueryAsync(query);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> DeleteHeading(int id)
+        {
+            await using var connection = new SqlConnection(_connectionString);
+            await connection.ExecuteAsync($"Delete From TblHeading Where HeadingID = '{id}'");
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateHeading(int id)
+        {
+            await using var connection = new SqlConnection(_connectionString);
+            var values = await connection.QueryFirstAsync<Headings>($"Select * From TblHeading Where HeadingID='{id}'");
+            return View(values);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateHeading(Headings headings)
+        {
+            await using var connection = new SqlConnection(_connectionString);
+            var query = $"UPDATE TblHeading set HeadingName='{headings.HeadingName}' , HeadingStatus ='{headings.HeadingStatus}' where HeadingID='{headings.HeadingID}'";
+            await connection.QueryAsync(query);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
